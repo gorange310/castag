15cb8846389600e0f28b63b55e943866abc31a77
Gene <gorange310@gmail.com>
2021-02-12 17:26:25 +0800

    cleanup layout

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,6 +1,10 @@
 class EpisodesController < ApplicationController
+  def index
+    @episodes = Episode.page(params[:page]).per(5).order(published_at: :desc)
+  end
+
   def search
     keyword = params[:keyword]
-    @episodes = Episode.search(keyword).page(params[:page]).per(5)
+    @episodes = Episode.search(keyword).page(params[:page]).per(5).order(published_at: :desc)
   end
 end
